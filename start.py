16311cf7979c3b3d5f702e8ae3a773fbac5abf5a
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-04-15 09:55:32 +0530

    Try

--- start.py
+++ start.py
@@ -1,4 +1,2 @@
-from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/Hobby-Dev-0/Andencento && cd Andencento && python3 -m userbot")
-# try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
+from os import system
+system ("git clone https://github.com/Hobby-Dev-0/X && cd X && python3 -m X")
